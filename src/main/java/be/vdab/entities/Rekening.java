239f38fb07b6211d80351aa22bfe58314a227d3e
Sinodaurus <sinodaurus@gmail.com>
2013-12-10 09:51:08 +0100

    javadoc

--- src/main/java/be/vdab/entities/Rekening.java
+++ src/main/java/be/vdab/entities/Rekening.java
@@ -2,8 +2,19 @@ package be.vdab.entities;
 
 import java.math.BigDecimal;
 
+/**
+ * Een rekening van een bank
+ *
+ * @author Sven
+ *
+ */
 public class Rekening {
-	private BigDecimal saldo;
+	private BigDecimal saldo = BigDecimal.ZERO;
+	/**
+	 * Het saldo ophalen
+	 * 
+	 * @return het saldo
+	 */
 	public BigDecimal getSaldo(){
 		return saldo;
 	}
